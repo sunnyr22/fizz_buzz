0971158997a2758b0f055bf357fbb1a119ecc4a2
Sundeep <sunnyr22@gmail.com>
2015-07-22 19:45:01 -0400

    simple fizz_buzz

---
+++ app.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	for (var i = 1; i <= 100; i++) {
+		if (i%3 === 0  && i%5 === 0) {
+			$('.list').append("<li>'fizzbuzz'</li>");
+		}
+
+	 	else if (i%3 === 0) {
+	 		$('.list').append("<li>" + 'fizz' + "</li>");
+		}
+
+		else if (i%5 === 0) {
+			$('.list').append("<li>" +'buzz' + "</li>");
+		}
+
+		else {
+			$('.list').append("<li>" + i +  "</li>");
+		}
+
+	}
+});
