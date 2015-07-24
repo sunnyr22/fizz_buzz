613eeed541299d95f91108dd06565b13ce482503
Sundeep <sunnyr22@gmail.com>
2015-07-23 21:47:15 -0400

    completed Fizz buzz

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
 
-	var h = prompt("Enter number between 1 to 100");
+	var h = +prompt("Enter any number");
+	
+
+
+     while (  isNaN(parseInt(h)) || h % 1 != 0) {
+     	h = prompt("Please enter a valid whole number between 1 to 100 ");
+
+     }
 	for (var i = 1; i <= h; i++) {
 		if (i%3 === 0  && i%5 === 0) {
 			$('.list').append('<li>' + 'Fizz-Buzz'+'</li>');
